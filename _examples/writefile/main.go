4fbdec7f63511305a70bf862d18c822bff4aaa54
wuriyanto <wuriyanto48@yahoo.co.id>
2019-03-13 11:45:01 +0700

    add example parse template and write to file

---
+++ _examples/writefile/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/Bhinneka/got"
+)
+
+// Person struct
+type Person struct {
+	ID   string
+	Name string
+}
+
+func main() {
+
+	data := Person{
+		ID:   "1",
+		Name: "Wuriyanto",
+	}
+
+	tmpl := `
+	Hello
+	Your ID : {{ .ID }}
+	Your Name : {{ toUpper .Name }}
+	`
+
+	funcMaps := template.FuncMap{
+		"toUpper": func(v string) string {
+			return strings.ToUpper(v)
+		},
+	}
+
+	// create file
+	output, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// close file
+	defer output.Close()
+
+	err = got.Parse(tmpl, data, output, funcMaps, got.ParseTemplateText)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
